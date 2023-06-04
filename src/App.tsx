a2517451ba53db03f553fc7f0ce425e3e2c99e7b
KwangjunLim <lkjun95324@naver.com>
2023-06-04 18:04:36 +0900

    커밋테스트 2

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <div>커밋테스트</div>
+      <div>커밋테스트2</div>
     </>
   );
 }
