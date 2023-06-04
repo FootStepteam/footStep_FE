ff680f2c02e9f4f69d3eba9f785c146bfdb8a43a
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-04 22:07:17 +0900

    chore: tailwind.config 수정
    content 수정, custom color 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{html,js,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-color': 'rgba(0, 175, 255, 0.4)',
+        'placeholder-color': 'A5A5A5',
+        'border-color': 'DCDCDC',
+        'white-color': 'ffffff',
+        'hover-color': 'rgba(0, 175, 255, 0.6)',
+      },
+    },
   },
   plugins: [],
-}
-
+};
