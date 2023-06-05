1b1ed07f889525f0beb9ae714f9d4de7fa1b5660
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-05 17:23:17 +0900

    design: Footer 컴포넌트 추가
    Footer 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Footer from './components/common/footer/Footer';
 import HeaderContainer from './components/common/header/HeaderContainer';
 
 function App() {
   return (
     <>
       <HeaderContainer />
+      <Footer />
     </>
   );
 }
---
+++ src/components/common/footer/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="flex justify-center items-center w-full h-36 bg-main-color">
+        <section className="flex flex-col justify-center items-center w-[40rem] h-10">
+          <div className="flex justify-center mb-6 font-bold text-white">
+            <div>ZERO-BASE</div>
+            <div>Github</div>
+          </div>
+          <p className="font-bold text-white">Copyright © Zerobase try-catch Team All Rights Reserved. </p>
+        </section>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
