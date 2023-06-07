d519e60524ebe17e1262384c9d8634129bbe0419
KwangjunLim <lkjun95324@naver.com>
2023-06-08 00:29:58 +0900

    Feat: CommunityPage 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { Route, Routes } from 'react-router';
-import MainPage from './pages/MainPage';
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
+import CommunityPage from "./pages/CommunityPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route
-          path="/"
-          element={<MainPage />}
-        />
+        <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/community" element={<CommunityPage />} />
       </Routes>
     </>
   );
