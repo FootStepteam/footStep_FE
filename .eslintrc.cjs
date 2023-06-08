03a8e0a55c149169dbe49481c5a3067598a05430
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-08 17:52:06 +0900

    Docs: eslint code rule 설정
    - eslint에 사용할 code rule 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,10 +4,19 @@ module.exports = {
     es2021: true,
     node: true,
   },
+  settings: {
+    // import/resolver` 는 `eslint-plugin-import` 의 경로 설정 옵션
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "airbnb",
     "plugin:prettier/recommended",
   ],
   overrides: [],
@@ -21,10 +30,40 @@ module.exports = {
   },
   plugins: ["react", "@typescript-eslint", "react-hooks", "prettier"],
   rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "react/react-in-jsx-scope": 0,
     "react/prefer-stateless-function": 0,
     "react/jsx-filename-extension": 0,
     "react/jsx-one-expression-per-line": 0,
+    "react/jsx-props-no-spreading": "off",
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
     "no-nested-ternary": 0,
+    "import/no-unresolved": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+        json: "never",
+      },
+    ],
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "jsx-a11y/label-has-associated-control": [
+      2,
+      {
+        labelAttributes: ["htmlFor"],
+      },
+    ],
   },
 };
