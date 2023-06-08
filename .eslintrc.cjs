1f198583c6a0ec6a9ac093e64ff4ad2a3c779d32
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-09 01:52:43 +0900

    Chore: endOfLine에 대한 rule 추가
    endOfLine에 대한 rule이 없어 오류 발생하여 rule 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,7 +36,7 @@ module.exports = {
     "react/jsx-filename-extension": 0,
     "react/jsx-one-expression-per-line": 0,
     "react/jsx-props-no-spreading": "off",
-    "prettier/prettier": "error",
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
     "arrow-body-style": "off",
     "prefer-arrow-callback": "off",
     "no-nested-ternary": 0,
