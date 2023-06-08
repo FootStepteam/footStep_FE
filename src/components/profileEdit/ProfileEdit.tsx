0f51240f1124713708951908e868e86953fbe914
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-08 14:36:48 +0900

    Design: 사용자 프로필 수정 페이지 추가
    - 사용자 프로필 수정 페이지 추가
    - ProdileEditForm : 프로필 입력 Form
    - Tab : tab
    - ProfileEdit : 프로필 수정 전체 컴포넌트

--- src/components/profileEdit/ProfileEdit.tsx
+++ src/components/profileEdit/ProfileEdit.tsx
@@ -1,5 +1,13 @@
+import ProfileEditForm from './ProfileEditForm';
+import Tab from './Tab';
+
 const ProfileEdit = () => {
-  return <></>;
+  return (
+    <>
+      <Tab />
+      <ProfileEditForm />
+    </>
+  );
 };
 
 export default ProfileEdit;
---
+++ src/components/profileEdit/Tab.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Tab = () => {
+  const [selected, setSelected] = useState('profileEdit');
+
+  const tabs = [
+    {
+      menu: 'profileEdit',
+      text: '회원정보수정',
+      path: '/user/profile/edit',
+    },
+    {
+      menu: 'passwordEdit',
+      text: '비밀번호변경',
+      path: '/user/profile/edit',
+    },
+    {
+      menu: 'userSecession',
+      text: '회원탈퇴',
+      path: '/user/profile/edit',
+    },
+  ];
+
+  const onClickHandler = (menu: string) => {
+    setSelected(menu);
+  };
+
+  return (
+    <div className="flex justify-center items-center w-full border-y border-[#DCDCDC]">
+      {tabs.map((tab) => (
+        <Link
+          className={`mx-2 px-2 py-4 text-md font- ${selected === tab.menu && 'border-b-2 border-skyblue-1 text-skyblue-1 text-lg font-bold'}`}
+          onClick={() => onClickHandler(tab.menu)}
+          to={tab.path}
+        >
+          {tab.text}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Tab;
