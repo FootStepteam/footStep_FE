e338d772c291a15decc63e228b2a063250f9b971
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-08 13:03:30 +0900

    Merge develop branch
    Merge develop branch

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './input.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
