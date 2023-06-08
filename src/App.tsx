0fd00f74ab84baa684ddb7cabc2ad3597e01c7d4
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-08 16:25:48 +0900

    Style: 코드 포맷 변경
    코드 포맷 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import LoginPage from "./pages/LoginPage";
-import UserProfilePage from "./pages/UserProfilePage";
-import CommunityPage from "./pages/CommunityPage";
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import CommunityPage from './pages/CommunityPage';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/community" element={<CommunityPage />} />
-        <Route path="/user/profile" element={<UserProfilePage />} />
+        <Route
+          path="/"
+          element={<MainPage />}
+        />
+        <Route
+          path="/login"
+          element={<LoginPage />}
+		/>
+        <Route
+          path="/community"
+          element={<CommunityPage />}
+        />
       </Routes>
     </>
   );
--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,6 +1,6 @@
-import Footer from '../components/common/footer/Footer';
-import HeaderContainer from '../components/common/header/HeaderContainer';
-import Profile from '../components/profile/Profile';
+import Footer from "../components/common/footer/Footer";
+import HeaderContainer from "../components/common/header/HeaderContainer";
+import Profile from "../components/profile/Profile";
 
 const UserProfilePage = () => {
   return (
