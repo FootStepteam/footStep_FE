b3182115d314df4ff6c6d9ccc26af6898fde8ed1
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-10 04:03:56 +0900

    Refactor: AboutPage로 파일명 변경하여 출력될 컴포넌트 수정
    AboutPage로 파일명 변경하여 출력될 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import ProfileEditPage from "./pages/ProfileEditPage";
 import CommunityPage from "./pages/CommunityPage";
 import PlanShareEntrancePage from "./pages/PlanShareEntrancePage";
+import AboutPage from "./pages/AboutPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<AboutPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/user/profile" element={<ProfilePage />} />
       <Route path="/user/profile/:path" element={<ProfileEditPage />} />
