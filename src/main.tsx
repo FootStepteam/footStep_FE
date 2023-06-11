afe5d5ee0310e892ab7abccffb137c48d13ab80a
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-12 00:12:42 +0900

    Design: 일정 공유방 생성 모달창 애니메이션 및 react-calendar custom css파일 추가
    - 일정 공유방 생성 모달창 애니메이션 추가
    - react-calendar custom css파일 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
+import "react-calendar/dist/Calendar.css";
+import "./calender.css";
 import "./input.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
