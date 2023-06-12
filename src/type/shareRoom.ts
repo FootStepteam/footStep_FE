f62d5559593b00fa684aae53370ec338ab867bad
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-12 23:32:20 +0900

    Feature: 공유코드로 검색 결과 컴포넌트 및 검색api, 공통 interface 추가
    공유코드로 검색 결과 컴포넌트 및 검색api, 공통 interface 추가

---
+++ src/type/shareRoom.ts
@@ -0,0 +1,10 @@
+export interface IShareRoom {
+  endPoint: string;
+  imageUrl: string;
+  shareCode: string;
+  shareId: number;
+  shareName: string;
+  startPoint: string;
+  travelEndDate: string;
+  travelStartDate: string;
+}
