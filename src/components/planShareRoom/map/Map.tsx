de9a313c0967a9374befef4662be0c12c7474bd9
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-13 18:51:00 +0900

    Feature: 지도 롹대, 축소 기능 추가
    지도 확대, 축소 기능 추가

--- src/components/planShareRoom/map/Map.tsx
+++ src/components/planShareRoom/map/Map.tsx
@@ -1,6 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Map = () => {
+  const [kakaoMap, setKakaoMap] = useState();
+
+  const zoomIn = () => {
+    const level = kakaoMap.getLevel();
+    kakaoMap.setLevel(level - 1);
+  };
+
+  const zoomOut = () => {
+    const level = kakaoMap.getLevel();
+    kakaoMap.setLevel(level + 1);
+  };
+
   useEffect(() => {
     const container = document.getElementById("map");
     // 지도를 담을 영역의 DOM 레퍼런스
@@ -11,9 +23,30 @@ const Map = () => {
     };
 
     const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+    console.log(map);
+    setKakaoMap(map);
   }, []);
 
-  return <div id="map" className="w-[100vw] h-[100vh]" />;
+  return (
+    <div id="map" className="relative w-[100vw] h-[100vh] z-[9000]">
+      <div className="flex flex-col absolute top-20 right-4 w-12 h-24 bg-white outline-none rounded-lg shadow-lg z-[9999]">
+        <button
+          type="button"
+          className="grow border-b border-b-gray font-bold text-xl"
+          onClick={zoomIn}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          className="grow font-bold text-xl"
+          onClick={zoomOut}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Map;
