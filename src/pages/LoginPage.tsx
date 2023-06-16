b1c8a5c1a7c33adceea0eac9d0763474ffe8ff13
KwangjunLim <lkjun95324@naver.com>
2023-06-16 17:12:53 +0900

    Refactor: 회원가입 링크 코드 정리

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -10,16 +10,7 @@ const LoginPage = () => {
         <LoginHeader />
         <EmailLoginForm />
         <KakaoLoginButton />
-<<<<<<< HEAD
         <SignUpLink />
-=======
-        <div className="flex justify-center mt-8">
-          <p>아직 회원이 아니신가요?</p>
-          <Link to="/user/signup">
-            <p className="ml-2 text-skyblue-1 font-medium">회원가입</p>
-          </Link>
-        </div>
->>>>>>> e972adc36b64fc59097692dc52c4c5bac4c4f205
       </div>
     </div>
   );
