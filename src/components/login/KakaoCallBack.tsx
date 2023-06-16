00f2636e996de8ad0cb056414acb6fa9ced6e600
KwangjunLim <lkjun95324@naver.com>
2023-06-17 00:44:54 +0900

    Refactor: 카카오 로그인 콜백함수로 분리

---
+++ src/components/login/KakaoCallBack.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { useLoginState } from "../../state/loginState";
+import axios from "axios";
+
+const KakaoCallBack = () => {
+  const { login } = useLoginState();
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const authCode = url.searchParams.get("code");
+
+    if (authCode) {
+      console.log(authCode);
+      const getAccessToken = async () => {
+        try {
+          const authResponse = await axios.post(
+            "http://43.200.76.174:8080/api/auth/kakao",
+            {
+              authorizationCode: authCode,
+            }
+          );
+          console.log(authResponse);
+
+          const accessToken = authResponse.data.jwtAccessToken;
+
+          // loginState에서 호출(jwtAccessToken header에 저장)
+          login(accessToken);
+
+          // console.log(authResponse.data);
+        } catch (error) {
+          // console.error(error);
+        }
+      };
+
+      getAccessToken();
+    }
+  }, []);
+  return <div>Redirecting...</div>;
+};
+
+export default KakaoCallBack;
