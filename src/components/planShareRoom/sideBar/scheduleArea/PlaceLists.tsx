25fed4ddda23108475de0cfc9e93095f1c74a263
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-16 20:08:48 +0900

    Feature: 사이드바 영역(일정, 장소) 영역 닫기 기능 추가
    사이드바 영역(일정, 장소) 영역 닫기 기능 추가

--- src/components/planShareRoom/sideBar/scheduleArea/PlaceLists.tsx
+++ src/components/planShareRoom/sideBar/scheduleArea/PlaceLists.tsx
@@ -1,4 +1,13 @@
+import { useRecoilState } from 'recoil';
+import { sidebarState } from '../../../../state/sidebarState';
+
 const PlaceLists = () => {
+  const [sidebarOpenState, setSidebarOpenState] = useRecoilState(sidebarState);
+
+  const onClickHandler = () => {
+    setSidebarOpenState({ ...sidebarOpenState, placeSearch: true });
+  };
+
   return (
     <div className="mt-[80px]">
       <div className="flex flex-col items-center justify-center text-gray-002 text-xl font-[500]">
@@ -8,6 +17,7 @@ const PlaceLists = () => {
       <button
         type="button"
         className="flex my-8 mx-auto px-8 py-6 rounded-3xl bg-gray-003 text-2xl text-white font-bold"
+        onClick={onClickHandler}
       >
         + 장소추가
       </button>
---
+++ src/state/sidebarState.ts
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+import { ISidebarState } from "../type/sideBar";
+
+const initialState:ISidebarState = {
+    schedule : true,
+    placeSearch: false
+}
+
+export const sidebarState = atom({
+    key: "sidebarState",
+    default: initialState
+})
