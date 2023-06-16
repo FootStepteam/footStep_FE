bfe7e8c4fab09b051dc8de4acf85a5b8e5f28b7a
Cho-SeongJu <122018331+Cho-SeongJu@users.noreply.github.com>
2023-06-17 01:45:59 +0900

    Feature: 장소검색 시 지역 선택 기능 추가
    - 장소 검색 시 지역 선택 기능 추가
    - 선택 시 선택된 지역 css 변경
    - 지역 선택하지 않고 장소 검색 시 return

--- src/components/planShareRoom/sideBar/placeSearchArea/placeSearchAreaHeader/PlaceSearchAreaHeader.tsx
+++ src/components/planShareRoom/sideBar/placeSearchArea/placeSearchAreaHeader/PlaceSearchAreaHeader.tsx
@@ -7,7 +7,7 @@ const PlaceSearchAreaHeader = ({ placeSearch }: IPropsPlaceSearch) => {
   return (
     <div className="flex flex-col w-planShareRoomSideBar h-planShareRoomHeader bg-blue-002">
       <PlaceSearchTitle />
-      <PlaceSearchCategory />
+      <PlaceSearchCategory placeSearch={placeSearch} />
       <PlaceSearchBar placeSearch={placeSearch} />
     </div>
   );
--- src/components/planShareRoom/sideBar/placeSearchArea/placeSearchAreaHeader/PlaceSearchCategory.tsx
+++ src/components/planShareRoom/sideBar/placeSearchArea/placeSearchAreaHeader/PlaceSearchCategory.tsx
@@ -1,28 +1,41 @@
-const PlaceSearchCategory = () => {
-  const categoriesFirstRow: string[] = [
-    "서울",
-    "경기",
-    "인천",
-    "부산",
-    "대구",
-    "울산",
-    "대전",
-    "강원",
-    "충남",
-    "충북",
-    "전남",
-    "전북",
-    "광주",
-    "경남",
-    "경북",
-    "제주",
-  ];
+import { useState } from "react";
+import { IPropsPlaceSearch } from "../../../../../type/shareRoom";
+import { useRecoilState } from "recoil";
+import { selectedArea } from "../../../../../state/shareRoom";
+
+const categoriesFirstRow: string[] = [
+  "서울",
+  "경기",
+  "인천",
+  "부산",
+  "대구",
+  "울산",
+  "대전",
+  "강원",
+  "충남",
+  "충북",
+  "전남",
+  "전북",
+  "광주",
+  "경남",
+  "경북",
+  "제주",
+];
+const PlaceSearchCategory = ({ placeSearch }: IPropsPlaceSearch) => {
+  const [selected, setSelected] = useRecoilState(selectedArea);
+
+  const onClickHandler = (category: string) => {
+    setSelected(category);
+    placeSearch(category)
+  }
+
   return (
     <div className="grid grid-cols-8 mt-6">
       {categoriesFirstRow.map((category) => (
         <p
           key={category}
-          className="flex justify-center items-center py-2 bg-white border border-gray-003 cursor-pointer"
+          className={`flex justify-center items-center py-2 cursor-pointer ${selected === category ? "text-white bg-platinum-001" : "bg-white text-black border border-gray-003"}`}
+          onClick={() => onClickHandler(category)}
           role="presentation"
         >
           {category}
