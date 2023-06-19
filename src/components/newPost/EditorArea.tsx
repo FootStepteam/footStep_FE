5b3b4c9aa8cb1145ef177c85cf847b3939d1b25e
KwangjunLim <lkjun95324@naver.com>
2023-06-19 22:20:09 +0900

    ✨ Feature: New Post 컴포넌트 구현(UI미완성)

---
+++ src/components/newPost/EditorArea.tsx
@@ -0,0 +1,76 @@
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useEditor } from "../../hooks/useEditor";
+
+const EditorArea = () => {
+  const {
+    title,
+    setTitle,
+    content,
+    setContent,
+    isPublic,
+    setIsPublic,
+    selectedPlan,
+    setSelectedPlan,
+    plans,
+    handleSelectPlan,
+    submitPost,
+  } = useEditor();
+
+  return (
+    <div className="min-h-screen p-10">
+      <div>
+        <select
+          value={selectedPlan ? selectedPlan.shareName : ""}
+          onChange={(e) => {
+            const selectedPlanName = e.target.value;
+            const selectedPlan = plans.find(
+              (plan) => plan.shareName === selectedPlanName
+            );
+            if (selectedPlan) {
+              handleSelectPlan(selectedPlan);
+            }
+          }}
+        >
+          {plans.map((plan) => (
+            <option key={plan.shareId} value={plan.shareName}>
+              {plan.shareName}
+            </option>
+          ))}
+        </select>
+      </div>
+      <input
+        type="text"
+        placeholder="Title"
+        onChange={(e) => setTitle(e.target.value)}
+        className="w-full p-2 mb-4 border-2 border-gray-002 rounded-md"
+      />
+      <label className="flex items-center mb-4">
+        <input
+          type="checkbox"
+          onChange={(e) => setIsPublic(e.target.checked)}
+          className="mr-2"
+        />
+        Public
+      </label>
+      <div className="w-full p-2 mb-4 border-2 border-gray-002 rounded-md">
+        <CKEditor
+          editor={ClassicEditor}
+          data={content}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            setContent(data);
+          }}
+        />
+      </div>
+
+      <button
+        onClick={submitPost}
+        className="w-[150px] p-2 bg-blue-002 text-white-001 rounded-md"
+      >
+        게시하기
+      </button>
+    </div>
+  );
+};
+export default EditorArea;
--- src/pages/NewPost.tsx
+++ src/pages/NewPost.tsx
@@ -1,69 +1,14 @@
-import { useState } from "react";
-import { useRecoilState } from "recoil";
-import axios from "axios";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import EditorArea from "../components/newPost/EditorArea";
+import HeaderContainer from "../components/common/header/HeaderContainer";
+import Footer from "../components/common/footer/Footer";
 
 const NewPost = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [isPublic, setIsPublic] = useState(false);
-  const [shareId, setShareId] = useState(0);
-
-  const submitPost = async () => {
-    try {
-      const response = await axios.post("/api/api/community", {
-        communityName: title,
-        communityPublicState: isPublic,
-        content: content,
-        shareId: shareId,
-      });
-
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
-    <div className="p-10">
-      <input
-        type="text"
-        placeholder="Title"
-        onChange={(e) => setTitle(e.target.value)}
-        className="w-full p-2 mb-4 border-2 border-gray-002 rounded-md"
-      />
-      <label className="flex items-center mb-4">
-        <input
-          type="checkbox"
-          onChange={(e) => setIsPublic(e.target.checked)}
-          className="mr-2"
-        />
-        Public
-      </label>
-      <div className="w-full p-2 mb-4 border-2 border-gray-002 rounded-md">
-        <CKEditor
-          editor={ClassicEditor}
-          data={content}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            setContent(data);
-          }}
-        />
-      </div>
-      <input
-        type="number"
-        placeholder="Share ID"
-        onChange={(e) => setShareId(Number(e.target.value))}
-        className="w-full p-2 mb-4 border-2 border-gray-002 rounded-md"
-      />
-      <button
-        onClick={submitPost}
-        className="w-[150px] p-2 bg-blue-002 text-white-001 rounded-md"
-      >
-        게시하기
-      </button>
-    </div>
+    <>
+      <HeaderContainer />
+      <EditorArea />
+      <Footer />
+    </>
   );
 };
 
