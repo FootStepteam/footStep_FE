53e24700cc30a43d7afcd4161d0133fc8dd3be8d
KwangjunLim <lkjun95324@naver.com>
2023-06-23 19:55:21 +0900

    ⚡️ Style: 로그아웃 확인 창 구현

--- src/components/common/header/headerRight/AfterLogin.tsx
+++ src/components/common/header/headerRight/AfterLogin.tsx
@@ -1,14 +1,26 @@
+//AfterLogin.tsx
 import { Link } from "react-router-dom";
 import { MouseEvent, FunctionComponent } from "react";
 import { ReactComponent as NoProfile } from "../../../../assets/smile.svg";
 import { useLoginState } from "../../../../hooks/useLoginState";
+import Swal from "sweetalert2";
 
 const AfterLogin: FunctionComponent = () => {
   const { logout } = useLoginState();
 
   const handleLogout = (event: MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
-    logout();
+
+    Swal.fire({
+      title: "로그아웃 하시겠습니까?",
+      showCancelButton: true,
+      confirmButtonText: "확인",
+      cancelButtonText: "취소",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        logout();
+      }
+    });
   };
 
   return (
