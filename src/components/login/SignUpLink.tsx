9b2257063267f98ac63763b002395ceb39e30596
KwangjunLim <lkjun95324@naver.com>
2023-06-30 02:57:05 +0900

    💄 Style: 로그인 페이지 UI 개선

--- src/components/login/SignUpLink.tsx
+++ src/components/login/SignUpLink.tsx
@@ -5,7 +5,7 @@ const SignUpLink = () => {
     <div className="flex justify-center mt-8 min-w-max">
       <p className="mix-w-max">아직 회원이 아니신가요?</p>
       <Link to="/user/signup">
-        <p className="ml-2 mix-w-max text-skyblue-1 font-medium">회원가입</p>
+        <p className="ml-2 mix-w-max text-sky-001 font-medium">회원가입</p>
       </Link>
     </div>
   );
