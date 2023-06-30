11680412873573446e82ec62f26f6e875e08aff7
KwangjunLim <lkjun95324@naver.com>
2023-06-30 14:10:34 +0900

    💄 Style: 프로필 페이지 UI 개선

--- src/components/profile/participatingPlan/ExistsParticipatingPlan.tsx
+++ src/components/profile/participatingPlan/ExistsParticipatingPlan.tsx
@@ -7,21 +7,21 @@ const ExistsParticipatingPlan = () => {
   const { plans } = useSharedRoom();
 
   return (
-    <div className="px-4 w-[55rem] h-[33rem] overflow-y-scroll">
+    <div className="w-[55rem] h-[40rem] border border-black-003 rounded-xl overflow-y-scroll">
       {plans.map((plan) => (
         <Link
           key={plan.shareId}
           to={`/planShareRoom/${plan.shareId}`}
           className="group flex mx-auto my-8 w-[47rem] h-[9rem] border border-gray-003 rounded-xl cursor-pointer"
         >
-          <div className="w-[15rem] h-[9rem] rounded-xl">
+          <div className="w-[15rem] rounded-xl">
             <img
-              className="rounded-xl object-contain"
-              src={plan.imageUrl || testImage} // Assuming plan object has an img property
+              className="rounded-xl min-h-full object-cover"
+              src={plan.imageUrl || testImage}
               alt="방썸네일"
             />
           </div>
-          <div className="ml-12 w-[32rem]">
+          <div className="ml-12 w-[31rem]">
             <div className="mt-4 font-bold text-2xl">{plan.shareName}</div>
             <div className="mt-1 text-lg">
               {plan.travelStartDate} ~ {plan.travelEndDate}
@@ -31,11 +31,7 @@ const ExistsParticipatingPlan = () => {
             </div>
           </div>
           <div className="flex justify-center items-center w-12 rounded-r-xl bg-blue-002 transition-all duration-100 ease-out group-hover:w-20">
-            <RightArrow
-              width={20}
-              height={20}
-              fill="#FFFFFF"
-            />
+            <RightArrow width={20} height={20} fill="#FFFFFF" />
           </div>
         </Link>
       ))}
