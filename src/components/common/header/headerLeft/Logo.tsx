3b2b369673f0e083f54d68d9c18887c51585b9e2
KwangjunLim <lkjun95324@naver.com>
2023-06-30 14:59:03 +0900

    💄 Style: 헤더 UI 개선

--- src/components/common/header/headerLeft/Logo.tsx
+++ src/components/common/header/headerLeft/Logo.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <Link to="/" className="mr-8 w-32 h-16 bg-blue-100">
+    <Link to="/" className="mr-8 w-32 h-16 rounded-full bg-blue-004">
       logo
     </Link>
   );
--- src/components/common/header/headerRight/AfterLogin.tsx
+++ src/components/common/header/headerRight/AfterLogin.tsx
@@ -1,14 +1,12 @@
-//AfterLogin.tsx
 import { Link } from "react-router-dom";
 import { MouseEvent, FunctionComponent } from "react";
-import { ReactComponent as NoProfile } from "../../../../assets/smile.svg";
 import { useLoginState } from "../../../../hooks/useLoginState";
 import Swal from "sweetalert2";
 
 const AfterLogin: FunctionComponent = () => {
   const { logout } = useLoginState();
 
-  const handleLogout = (event: MouseEvent<HTMLAnchorElement>) => {
+  const handleLogout = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
 
     Swal.fire({
@@ -24,20 +22,30 @@ const AfterLogin: FunctionComponent = () => {
   };
 
   return (
-    <div className="flex justify-center items-center font-semibold">
-      <Link
-        to="/user/profile"
-        className="mr-5"
-      >
-        마이페이지
-      </Link>
-      <Link
-        to="/"
-        className="mr-5"
-        onClick={handleLogout}
-      >
-        로그아웃
-      </Link>
+    <div className="flex justify-center items-center h-[4rem] font-semibold">
+      <div>
+        <button
+          type="button"
+          className={`px-4 py-2 h-[4rem] border-r hover:bg-sky-005 hover:text-white-001 transition-all duration-150 ${
+            location.pathname === "/"
+              ? "hover:bg-white-001 hover:text-black-001"
+              : ""
+          }`}
+        >
+          <Link to="/user/profile">마이페이지</Link>
+        </button>
+        <button
+          type="button"
+          className={`px-4 py-2 h-[4rem] hover:bg-sky-005 hover:text-white-001 transition-all duration-150 ${
+            location.pathname === "/"
+              ? "hover:bg-white-001 hover:text-black-001"
+              : ""
+          }`}
+          onClick={handleLogout}
+        >
+          로그아웃
+        </button>
+      </div>
     </div>
   );
 };
