242dea467f35a34be572f0657b86a4878105dc94
KwangjunLim <lkjun95324@naver.com>
2023-07-01 19:21:00 +0900

    💄 Style: Header UI 수정

--- src/components/common/header/headerLeft/Menu.tsx
+++ src/components/common/header/headerLeft/Menu.tsx
@@ -59,9 +59,7 @@ const Menu = () => {
         <div
           key={element.menu}
           onClick={() => onClickHandler(element.path)}
-          className={`flex grow justify-center items-center text-[1.3rem] font-semibold cursor-pointer transition-all duration-150 ${
-            index !== menus.length - 1 ? "border-r" : ""
-          } ${
+          className={`flex grow justify-center items-center text-[1.3rem] rounded-lg font-semibold cursor-pointer transition-all duration-150 ${
             location.pathname === "/"
               ? "hover:bg-white-001 hover:text-black-001"
               : "hover:bg-sky-005 hover:text-white-001"
