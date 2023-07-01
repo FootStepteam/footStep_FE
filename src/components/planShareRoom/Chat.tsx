15c759bc2255169e2096c1137ec527de87f0d5f2
KwangjunLim <lkjun95324@naver.com>
2023-07-01 14:54:10 +0900

    ⚡️ Feature: 채팅 기능 구현 개선 및 UI작업

--- src/components/planShareRoom/Chat.tsx
+++ src/components/planShareRoom/Chat.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState, useEffect } from "react";
+import { ChangeEvent, useState, useEffect, useRef } from "react";
 import { ReactComponent as ChatEmotion } from "../../assets/chatEmotion.svg";
 import { ReactComponent as Close } from "../../assets/close.svg";
 // import SockJS from "sockjs-client";
@@ -6,7 +6,6 @@ import { Stomp } from "@stomp/stompjs";
 import { getMemberByAccessToken } from "../../api/memberAPI";
 import { useRecoilValue } from "recoil";
 import { shareRoomInfo } from "../../store/shareRoomInfo";
-import { getChatRoomDetail } from "../../api/chatAPI";
 
 interface IChatMessage {
   nickName: string;
@@ -15,29 +14,24 @@ interface IChatMessage {
   type: "ENTER" | "JOIN" | "TALK";
 }
 
-const colors = [
-  "#2196F3",
-  "#32c787",
-  "#00BCD4",
-  "#ff5652",
-  "#ffc107",
-  "#ff85af",
-  "#FF9800",
-  "#39bbb0",
-];
-
 const Chat = () => {
-  const [stompClient, setStompClient] = useState<any | null>(null);
+  const stompClient = useRef<any | null>(null);
   const [openChatStatus, setOpenChatStatus] = useState<boolean>(false);
   const [message, setMessage] = useState<string>("");
   const [messages, setMessages] = useState<IChatMessage[]>([]);
   const [username, setUsername] = useState<string>("");
   const shareRoomData = useRecoilValue(shareRoomInfo);
-  const shareId = shareRoomData.shareId;
+  const shareId: number = shareRoomData.shareId;
+  const [composing, setComposing] = useState(false);
+  const messagesEndRef = useRef<null | HTMLDivElement>(null);
 
   const onClickChatStatusHandler = () => {
+    if (!openChatStatus) {
+      connect();
+    } else {
+      disconnect();
+    }
     setOpenChatStatus(!openChatStatus);
-    connect();
   };
 
   const onChangeMessageHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
@@ -51,36 +45,30 @@ const Chat = () => {
       setUsername(userName);
 
       if (shareId) {
-        const roomDetail = await getChatRoomDetail(shareId);
         console.log(shareId);
-      }
-
-      if (userName && shareId) {
-        connect();
       }
     };
 
     fetchDetails()
       .then(() => console.log("fetchDetails has completed"))
       .catch((error) => console.error("Failed to fetch details:", error));
-
-    return () => {
-      disconnect();
-    };
   }, [shareId]);
 
   const connect = () => {
     const socketFactory = () =>
       new window.SockJS("http://43.200.76.174:8080/ws-stomp");
     const client = Stomp.over(socketFactory);
-    setStompClient(client);
+    stompClient.current = client; // Update the stompClient reference
     client.connect({}, onConnected, onError);
   };
 
   const onConnected = () => {
-    stompClient?.subscribe(`/sub/share-room/${shareId}`, onMessageReceived);
+    stompClient.current?.subscribe(
+      `/sub/share-room/${shareId}`,
+      onMessageReceived
+    );
 
-    stompClient?.send(
+    stompClient.current?.send(
       "/pub/chat/message",
       {},
       JSON.stringify({ shareId: shareId, nickName: username, type: "ENTER" })
@@ -95,7 +83,7 @@ const Chat = () => {
   };
 
   const sendMessageHandler = () => {
-    if (message && stompClient) {
+    if (message && stompClient.current) {
       const chatMessage = {
         shareId: shareId,
         nickName: username!,
@@ -103,9 +91,28 @@ const Chat = () => {
         type: "TALK",
       };
 
-      stompClient.send("/pub/chat/message", {}, JSON.stringify(chatMessage));
+      stompClient.current.send(
+        "/pub/chat/message",
+        {},
+        JSON.stringify(chatMessage)
+      );
       setMessage("");
     }
+  };
+
+  const onEnterPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (!composing && e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      sendMessageHandler();
+    }
+  };
+
+  const onCompositionStart = () => {
+    setComposing(true);
+  };
+
+  const onCompositionEnd = () => {
+    setComposing(false);
   };
 
   const onMessageReceived = (payload: any) => {
@@ -113,28 +120,22 @@ const Chat = () => {
 
     if (message.type === "JOIN") {
       message.message = message.nickName + " joined!";
-    } else if (message.type === "LEAVE") {
-      message.message = message.nickName + " left!";
     }
 
     setMessages((prevMessages) => [...prevMessages, message]);
   };
 
   const disconnect = () => {
-    if (stompClient && stompClient.connected) {
-      stompClient.disconnect();
+    if (stompClient.current && stompClient.current.connected) {
+      stompClient.current.disconnect();
     }
   };
 
-  const getAvatarColor = (messageSender: string) => {
-    let hash = 0;
-    for (let i = 0; i < messageSender.length; i++) {
-      hash = 31 * hash + messageSender.charCodeAt(i);
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
-
-    const index = Math.abs(hash % colors.length);
-    return colors[index];
-  };
+  }, [messages]);
 
   return (
     <>
@@ -157,29 +158,64 @@ const Chat = () => {
             />
           </div>
           <div className="flex flex-col mt-3 m-center w-[18rem] h-[30rem] bg-blue-005 rounded-md">
-            <div className="overflow-y-scroll no-scrollbar mt-3 ml-3 mr-3 mb-3 h-[25rem] bg-blue-007 rounded-md">
+            <div className="flex flex-col overflow-y-scroll no-scrollbar m-3 h-[25rem] bg-blue-005 rounded-md">
               {messages.map((msg, index) => (
-                <div key={index} className="mt-3 ml-3 text-white">
-                  <i style={{ backgroundColor: getAvatarColor(msg.nickName) }}>
-                    {msg.nickName[0]}
-                  </i>
-                  <span>{msg.nickName}</span>
-                  <p>{msg.message}</p>
+                <div
+                  key={index}
+                  className={`w-fit text-black ${
+                    msg.nickName === username
+                      ? "text-right place-self-end"
+                      : "place-self-start"
+                  } ${
+                    index > 0 && messages[index - 1].nickName === msg.nickName
+                      ? ""
+                      : "mt-3"
+                  }`}
+                >
+                  <span
+                    className={`text-sm ${
+                      msg.nickName === username
+                        ? "mr-1 place-self-end"
+                        : "ml-1 place-self-start"
+                    } ${
+                      index > 0 && messages[index - 1].nickName === msg.nickName
+                        ? "hidden"
+                        : ""
+                    }`}
+                  >
+                    {msg.nickName}
+                  </span>
+                  <p
+                    className={`px-2 py-1 mt-1 w-fit rounded-lg ${
+                      msg.nickName === username
+                        ? "bg-yellow-004"
+                        : "bg-green-005"
+                    }`}
+                  >
+                    {msg.message}
+                  </p>
                 </div>
               ))}
+              <div ref={messagesEndRef} />
             </div>
-            <textarea
-              placeholder="내용을 입력해주세요"
-              value={message}
-              onChange={onChangeMessageHandler}
-              className="mt-3 ml-3 mr-3 h-[4rem] rounded-md"
-            />
-            <div
-              role="button"
-              onClick={sendMessageHandler}
-              className="m-3 h-[2rem] rounded-md bg-blue-004 hover:bg-blue-003 cursor-pointer text-white flex justify-center items-center"
-            >
-              전송
+            <div className="flex">
+              <textarea
+                placeholder="내용을 입력해주세요"
+                value={message}
+                onChange={onChangeMessageHandler}
+                onKeyDown={onEnterPress}
+                onCompositionStart={onCompositionStart}
+                onCompositionEnd={onCompositionEnd}
+                className="p-1 mb-2 ml-3 w-full h-[4rem] rounded-l-lg"
+                style={{ resize: "none" }}
+              />
+              <button
+                type="button"
+                onClick={sendMessageHandler}
+                className="flex justify-center items-center px-2 mb-2 mr-2 min-w-max h-[4rem] rounded-r-lg bg-yellow-004 text-orange-001 hover:bg-yellow-003 cursor-pointer transition-all duration-150"
+              >
+                전송
+              </button>
             </div>
           </div>
         </div>
@@ -189,293 +225,3 @@ const Chat = () => {
 };
 
 export default Chat;
-
-// import { Stomp } from "@stomp/stompjs";
-// import React, { useState, useEffect, useRef } from "react";
-// import SockJS from "sockjs-client";
-
-// interface IChatMessage {
-//   sender: string;
-//   content: string;
-//   type: "JOIN" | "CHAT" | "LEAVE";
-// }
-
-// const colors = [
-//   "#2196F3",
-//   "#32c787",
-//   "#00BCD4",
-//   "#ff5652",
-//   "#ffc107",
-//   "#ff85af",
-//   "#FF9800",
-//   "#39bbb0",
-// ];
-
-// const Chat = () => {
-//   const [stompClient, setStompClient] = useState<any | null>(null);
-//   const [username, setUsername] = useState<string | null>(null);
-//   const [message, setMessage] = useState<string>("");
-//   const messageArea = useRef<HTMLUListElement>(null);
-//   const [messages, setMessages] = useState<IChatMessage[]>([]);
-
-//   const connect = (event: React.FormEvent) => {
-//     event.preventDefault();
-
-//     if (username) {
-//       const socket = new window.SockJS("http://43.200.76.174:8080/javatechie");
-//       const client = Stomp.over(socket);
-//       setStompClient(client);
-//       client.connect({}, onConnected, onError);
-//     }
-//   };
-
-//   const onConnected = () => {
-//     stompClient?.subscribe(
-//       "http://43.200.76.174:8080/topic/public",
-//       onMessageReceived
-//     );
-
-//     stompClient?.send(
-//       "http://43.200.76.174:8080/app/chat.register",
-//       {},
-//       JSON.stringify({ sender: username, type: "JOIN" })
-//     );
-//   };
-
-//   const onError = (error: any) => {
-//     console.error(
-//       "Could not connect to WebSocket server. Please refresh this page to try again!",
-//       error
-//     );
-//   };
-//   const sendMessage = (event: React.FormEvent) => {
-//     event.preventDefault();
-
-//     if (message && stompClient) {
-//       const chatMessage: IChatMessage = {
-//         sender: username!,
-//         content: message,
-//         type: "CHAT",
-//       };
-
-//       stompClient.send(
-//         "http://43.200.76.174:8080/app/chat.send",
-//         {},
-//         JSON.stringify(chatMessage)
-//       );
-//     }
-//   };
-
-//   const onMessageReceived = (payload: any) => {
-//     const message: IChatMessage = JSON.parse(payload.body);
-
-//     if (message.type === "JOIN") {
-//       message.content = message.sender + " joined!";
-//     } else if (message.type === "LEAVE") {
-//       message.content = message.sender + " left!";
-//     }
-
-//     setMessages((prevMessages) => [...prevMessages, message]);
-//   };
-
-//   const getAvatarColor = (messageSender: string) => {
-//     let hash = 0;
-//     for (let i = 0; i < messageSender.length; i++) {
-//       hash = 31 * hash + messageSender.charCodeAt(i);
-//     }
-
-//     const index = Math.abs(hash % colors.length);
-//     return colors[index];
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={connect}>
-//         <input
-//           type="text"
-//           onChange={(e) => setUsername(e.target.value)}
-//           placeholder="Enter your name..."
-//         />
-//         <button type="submit">Connect</button>
-//       </form>
-//       <form onSubmit={sendMessage}>
-//         <input
-//           type="text"
-//           onChange={(e) => setMessage(e.target.value)}
-//           placeholder="Type a message..."
-//         />
-//         <button type="submit">Send</button>
-//       </form>
-//       <ul ref={messageArea}>
-//         {messages.map((message, i) => (
-//           <li key={i} className={message.type}>
-//             <i style={{ backgroundColor: getAvatarColor(message.sender) }}>
-//               {message.sender[0]}
-//             </i>
-//             <span>{message.sender}</span>
-//             <p>{message.content}</p>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Chat;
-
-//websocket 버전
-// import { ChangeEvent, useState, useEffect } from "react";
-// import { ReactComponent as ChatEmotion } from "../../assets/chatEmotion.svg";
-// import { ReactComponent as Close } from "../../assets/close.svg";
-// import SockJS from "sockjs-client";
-// import { Stomp, Client } from "@stomp/stompjs";
-// import { getMemberByAccessToken } from "../../api/memberAPI";
-// import { useRecoilValue } from "recoil";
-// import { shareRoomInfo } from "../../store/shareRoomInfo";
-// import { getChatRoomDetail, getChatRooms } from "../../api/chatAPI";
-
-// let stompClient: any;
-
-// const Chat = () => {
-//   const [openChatStatus, setOpenChatStatus] = useState<boolean>(false);
-//   const [message, setMessage] = useState<string>("");
-//   const [messages, setMessages] = useState<string[]>([]);
-//   const [roomId, setRoomId] = useState<string>("");
-//   const [username, setUsername] = useState<string>("");
-//   const [loadingUsername, setLoadingUsername] = useState<boolean>(true);
-//   const shareRoomData = useRecoilValue(shareRoomInfo);
-//   const shareId = shareRoomData.shareId;
-
-//   const onClickChatStatusHandler = () => {
-//     setOpenChatStatus(!openChatStatus);
-//   };
-
-//   const onChangeMessageHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
-//     setMessage(e.target.value);
-//   };
-
-//   const sendMessageHandler = () => {
-//     if (message.trim() !== "") {
-//       const chatMessage = {
-//         roomId: roomId,
-//         sender: username,
-//         message: message.trim(),
-//       };
-//       stompClient.publish({
-//         destination: "/app/chat/sendMessage",
-//         body: JSON.stringify(chatMessage),
-//       });
-//       setMessage("");
-//     }
-//   };
-
-//   useEffect(() => {
-//     const fetchDetails = async () => {
-//       const memberData = await getMemberByAccessToken();
-//       const userName = memberData.nickname;
-//       setUsername(userName);
-//       setLoadingUsername(false);
-//       console.log("username: ", userName); // 이곳에선 최신 값을 얻을 수 있습니다.
-
-//       const roomId = shareId.toString();
-//       setRoomId(roomId);
-
-//       if (roomId) {
-//         console.log("roomId: ", roomId);
-//         const roomDetail = await getChatRoomDetail(roomId);
-//       }
-
-//       if (userName && roomId) {
-//         console.log("roomId: ", roomId);
-//         console.log("loadingUsername: ", loadingUsername);
-//         connect(); // 함수에 직접 인자를 전달합니다.
-//       }
-//     };
-
-//     fetchDetails()
-//       .then(() => console.log("fetchDetails has completed"))
-//       .catch((error) => console.error("Failed to fetch details:", error));
-
-//     return () => {
-//       disconnect();
-//     };
-//   }, [shareId]);
-
-//   const connect = () => {
-//     stompClient = Stomp.over(new WebSocket("ws://43.200.76.174:8080/ws-stomp"));
-
-//     const onConnect = () => {
-//       console.log("Connected");
-//       stompClient.subscribe(`/sub/chat/room/${roomId}`, onMessageReceived);
-//     };
-
-//     const onError = (error: any) => {
-//       console.log("Connection failed:", error);
-//     };
-
-//     stompClient.connect({}, onConnect, onError);
-//     console.log("stompClient: ", stompClient);
-//     console.log("Activating stompClient");
-//     stompClient.activate();
-//   };
-
-//   const onMessageReceived = (payload: any) => {
-//     const msg = JSON.parse(payload.body);
-//     setMessages((prevMsgs) => [...prevMsgs, msg.message]);
-//   };
-
-//   const disconnect = () => {
-//     if (stompClient && stompClient.active) {
-//       stompClient.deactivate();
-//     }
-//   };
-
-//   return (
-//     <>
-//       <div
-//         role="button"
-//         onClick={onClickChatStatusHandler}
-//         className="absolute bottom-10 right-10 w-[5rem] h-[5rem] rounded-full hover:bg-gray-003 cursor-pointer z-[1016]"
-//       >
-//         <ChatEmotion className="w-[5rem] h-[5rem]" />
-//       </div>
-//       {openChatStatus && (
-//         <div
-//           className={`absolute right-5 bottom-10 w-[20rem] h-[35rem] bg-black-002 rounded-2xl z-[1017] ]`}
-//         >
-//           <div className="flex flex-row-reverse mt-3 mr-4">
-//             <Close
-//               role="button"
-//               onClick={onClickChatStatusHandler}
-//               className="w-[25px] h-[25px] fill-white"
-//             />
-//           </div>
-//           <div className="flex flex-col mt-3 m-center w-[18rem] h-[30rem] bg-blue-005 rounded-md">
-//             <div className="overflow-y-scroll no-scrollbar mt-3 ml-3 mr-3 mb-3 h-[25rem] bg-blue-007 rounded-md">
-//               {messages.map((msg, index) => (
-//                 <div key={index} className="mt-3 ml-3 text-white">
-//                   {msg}
-//                 </div>
-//               ))}
-//             </div>
-//             <textarea
-//               placeholder="내용을 입력해주세요"
-//               value={message}
-//               onChange={onChangeMessageHandler}
-//               className="mt-3 ml-3 mr-3 h-[4rem] rounded-md"
-//             />
-//             <div
-//               role="button"
-//               onClick={sendMessageHandler}
-//               className="m-3 h-[2rem] rounded-md bg-blue-004 hover:bg-blue-003 cursor-pointer text-white flex justify-center items-center"
-//             >
-//               전송
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//     </>
-//   );
-// };
-
-// export default Chat;
