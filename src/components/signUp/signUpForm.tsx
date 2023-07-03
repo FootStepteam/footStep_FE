a5ea7f11436bbf46658a7c496478c70b1bef0320
Cho-SeongJu <tjdwn9753@gmail.com>
2023-07-04 01:16:19 +0900

    💄 Design: 로그인, 회원가입 페이지 컴포넌트 UI 수정
    로그인, 회원가입 페이지 컴포넌트 UI 수정

--- src/components/signUp/signUpForm.tsx
+++ src/components/signUp/signUpForm.tsx
@@ -1,8 +1,10 @@
 import { FormEvent, useState } from "react";
 import { useSignUpForm } from "../../store/useSignUpForm";
 import { signUp, checkEmailDuplication } from "../../api/userSignUp";
-import Swal from "sweetalert2";
+import { kakaoLogin } from "../../api/kakaoLoginAPI";
 import { useNavigate } from "react-router-dom";
+import { ReactComponent as Kakao } from "../../assets/kakao.svg";
+import Swal from "sweetalert2";
 
 const SignUpForm = () => {
   const { formData, updateForm } = useSignUpForm();
@@ -101,75 +103,104 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className="pt-[112px]">
+    <div className="mt-[10rem] mx-auto w-[20rem]">
+      <h1 className="mb-12 text-black-002 font-bold text-xl">회원가입</h1>
+      <div className="mb-8">
+        <p className="flex justify-center mb-2 text-[0.85rem] text-gray-001">
+          카카오계정으로 간편 회원가입
+        </p>
+        <button
+          className="flex justify-center items-center mx-auto w-[20rem] h-[3.2rem] bg-yellow-001 hover:bg-yellow-002 rounded-md text-white font-bold"
+          onClick={kakaoLogin}
+          type="button"
+        >
+          <Kakao
+            width={25}
+            height={25}
+            className="mr-2"
+          />
+          카카오계정으로 회원가입
+        </button>
+      </div>
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col w-full h-screen max-w-md mx-auto mt-10 p-6 space-y-4 rounded-lg bg-white-001 shadow-lg"
+        className="flex flex-col mx-auto w-[20rem] h-screen bg-white"
       >
-        <div className="flex p-2 mx-auto space-x-2 border border-gray-003 rounded-lg text-black-002 text-lg">
-          <p className="mr-8">성별</p>
-          <label>
-            <input
-              type="radio"
-              name="gender"
-              value="male"
-              checked={formData.gender === "male"}
-              onChange={updateForm}
-              className="mr-2"
-            />
-            남자
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="gender"
-              value="female"
-              checked={formData.gender === "female"}
-              onChange={updateForm}
-              className="mr-2"
-            />
-            여자
-          </label>
-        </div>
-        <div className="flex">
+        <div className="mb-6">
+          <label className="block mb-3 font-bold text-black-002">이메일</label>
           <input
             type="email"
             name="loginEmail"
             value={formData.loginEmail}
             onChange={updateForm}
             disabled={emailValid}
-            className="block w-full p-2 border border-gray-003 rounded-l-lg placeholder-gray-001 font-NanumGothic"
-            placeholder="이메일"
+            className="w-full px-4 py-3 border border-gray-003 outline-none rounded-md placeholder:text-gray-002 placeholder:text-sm"
+            placeholder="이메일을 입력하세요"
           />
-          <button
-            type="button"
-            onClick={handleCheckEmail}
-            disabled={emailValid}
-            className="p-1 min-w-max rounded-r-lg bg-blue-003 text-white-001"
-          >
-            중복
-            <br />
-            확인
-          </button>
         </div>
-        <input
-          name="nickname"
-          value={formData.nickname}
-          onChange={updateForm}
-          className="block w-full p-2 border border-gray-003 rounded-lg placeholder-gray-001 font-NanumGothic"
-          placeholder="닉네임"
-        />
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={updateForm}
-          className="block w-full p-2 border border-gray-003 rounded-lg placeholder-gray-001 font-NanumGothic"
-          placeholder="비밀번호"
-        />
+        <div className="mb-6">
+          <label className="block mb-3 font-bold text-black-002">
+            비밀번호
+          </label>
+          <p className="mb-2 text-[0.75rem] text-gray-001">
+            영문, 숫자, 특수문자를 포함한 8~16자 비밀번호를 입력해주세요.
+          </p>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={updateForm}
+            className="w-full px-4 py-3 border border-gray-003 outline-none rounded-md placeholder:text-gray-002 placeholder:text-sm"
+            placeholder="비밀번호"
+            maxLength={16}
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block mb-3 font-bold text-black-002">
+            비밀번호 확인
+          </label>
+          <input
+            type="password"
+            name="checkPassword"
+            value={formData.password}
+            onChange={updateForm}
+            className="w-full px-4 py-3 border border-gray-003 outline-none rounded-md placeholder:text-gray-002 placeholder:text-sm"
+            placeholder="비밀번호 확인"
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block mb-3 font-bold text-black-002">닉네임</label>
+          <p className="mb-2 text-[0.8rem] text-gray-001">
+            닉네임을 2자 이상 10자 이하로 입력해주세요.
+          </p>
+          <input
+            name="nickname"
+            value={formData.nickname}
+            onChange={updateForm}
+            className="w-full px-4 py-3 border border-gray-003 outline-none rounded-md placeholder:text-gray-002 placeholder:text-sm"
+            placeholder="별명 (2~10자)"
+          />
+        </div>
+        <div className="mb-12">
+          <p className="mb-3 font-bold text-black-002">성별</p>
+          <div className="flex w-full">
+            <button
+              type="button"
+              className="grow py-3 border border-gray-003 hover:border-blue-004 rounded-l-md hover:bg-blue-004 font-bold hover:text-white transition-colors duration-100"
+            >
+              남성
+            </button>
+            <button
+              type="button"
+              className="grow py-3 border-t border-r border-b border-gray-003 hover:border-blue-004 font-bold hover:text-white rounded-r-md hover:bg-blue-004 transition-colors duration-100"
+            >
+              여성
+            </button>
+          </div>
+        </div>
         <button
           type="submit"
-          className="block w-full p-2 bg-blue-002 text-white-001 rounded-lg hover:bg-blue-003 transition-colors duration-200 font-DoHyeon text-lg"
+          className="block w-full p-3 bg-blue-002 text-white-001 rounded-md hover:bg-blue-003 text-lg font-bold transition-colors duration-100"
         >
           회원가입
         </button>
