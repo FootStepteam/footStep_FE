4c40211131316d28a605c64e1ee4acd96bbd0976
Cho-SeongJu <tjdwn9753@gmail.com>
2023-07-05 03:21:11 +0900

    👽️ !BREAKING CHANGE: API 변경으로 인해 type 수정
    API 변경으로 인해 type 수정

--- src/store/getShareRoomList.ts
+++ src/store/getShareRoomList.ts
@@ -1,9 +1,12 @@
 import { atom } from "recoil";
-import { IShareRoom } from "../type/shareRoom";
+import { IShareRoomList } from "../type/shareRoom";
 
-const initial:IShareRoom[] = [];
+const initial: IShareRoomList = {
+  totalPages: -1,
+  shareRoomDtoList: [],
+};
 
 export const getShareRoomList = atom({
-    key: "getShareRoomList",
-    default: initial,
-})
+  key: "getShareRoomList",
+  default: initial,
+});
