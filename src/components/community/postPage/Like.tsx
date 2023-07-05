94454abbd9080054e75f611c4ca16e0f4af507e7
Cho-SeongJu <tjdwn9753@gmail.com>
2023-07-05 21:07:23 +0900

    🐛 Fix: 좋아요 오류 수정
    좋아요 오류 수정

--- src/components/community/postPage/Like.tsx
+++ src/components/community/postPage/Like.tsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
-import { likePostAPI, unlikePostAPI } from "../../../api/postAPI";
-import { ILikeProps } from "../../../type/communityPage";
-import { ReactComponent as Heart } from "../../../assets/heartNoFill.svg";
-import { ReactComponent as HeartFill } from "../../../assets/heartFill.svg";
 import Swal from "sweetalert2";
+import { likePostAPI, unlikePostAPI } from "../../../api/postAPI";
+import { ReactComponent as HeartFill } from "../../../assets/heartFill.svg";
+import { ReactComponent as Heart } from "../../../assets/heartNoFill.svg";
+import { ILikeProps } from "../../../type/communityPage";
 import { getCookie } from "../../../utils/cookie";
 
-const Like = ({ communityId, initialLikeCount }: ILikeProps) => {
+const Like = ({ isLiked, communityId, initialLikeCount }: ILikeProps) => {
   const [likeCount, setLikeCount] = useState(initialLikeCount);
-  const [isLiked, setIsLiked] = useState(false);
+  const [liked, setLiked] = useState<boolean>(isLiked);
 
   const handleLike = async () => {
     const KEY = "accessToken";
     const token = getCookie(KEY);
-    // 비로그인 좋아요시 처리
+
     if (!token) {
       Swal.fire({
         title: "로그인 후 이용 가능합니다.",
@@ -22,27 +22,36 @@ const Like = ({ communityId, initialLikeCount }: ILikeProps) => {
       });
       return;
     }
-    // 정상 처리
-    if (isLiked) {
+
+    if (liked) {
       await unlikePostAPI(communityId);
       setLikeCount(likeCount - 1);
+      setLiked(false);
     } else {
       await likePostAPI(communityId);
       setLikeCount(likeCount + 1);
+      setLiked(true);
     }
-    setIsLiked(!isLiked);
   };
 
   return (
     <div className="flex items-center mb-2">
       <button
-        className={`mr-2 focus:outline-none ${isLiked ? "animate-jump" : ""}`}
+        className={`mr-2 focus:outline-none ${liked ? "animate-jump" : ""}`}
         onClick={handleLike}
       >
-        {isLiked ? (
-          <HeartFill width={20} height={20} className="text-red-001" />
+        {liked ? (
+          <HeartFill
+            width={20}
+            height={20}
+            className="text-red-001"
+          />
         ) : (
-          <Heart width={20} height={20} className="text-black-002" />
+          <Heart
+            width={20}
+            height={20}
+            className="text-black-002"
+          />
         )}
       </button>
       <p className="text-gray-001">좋아요 {likeCount} 개</p>
