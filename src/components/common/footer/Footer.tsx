7428f18c4f4a23ef52c0e9372de83ff9853feee8
KwangjunLim <lkjun95324@naver.com>
2023-07-24 16:00:15 +0900

    💄 Style: footer 반응형 웹 적용

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -2,11 +2,11 @@ import FooterLink from "./FooterLink";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center w-full h-60 bg-black-002">
-      <section className="flex flex-col justify-center items-center w-[40rem] h-32">
+    <footer className="flex justify-center items-center px-4 md:px-0 w-full h-60 bg-black-002">
+      <section className="flex flex-col justify-center items-center w-full md:w-[40rem] h-32">
         <FooterLink />
-        <p className="font-bold text-white">
-          Copyright © Zerobase try-catch Team All Rights Reserved.{" "}
+        <p className="font-bold text-center md:text-left text-white">
+          Copyright © Zerobase try-catch Team All Rights Reserved.
         </p>
       </section>
     </footer>
