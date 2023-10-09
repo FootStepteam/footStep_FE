e689ae4cff41e1c56a90aafcb95d2fa335d76419
Cho-SeongJu <tjdwn9753@gmail.com>
2023-10-09 18:53:54 +0900

    🐛 Fix : 헤더 배경 오류 수정
    - 헤더 배경 오류 수정

--- src/components/common/header/HeaderContainer.tsx
+++ src/components/common/header/HeaderContainer.tsx
@@ -1,21 +1,12 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
 import LeftContainer from "./headerLeft/LeftContainer";
 import RightContainer from "./headerRight/RightContainer";
 
 const HeaderContainer = () => {
-  const [path, setPath] = useState("/");
-  const location = useLocation();
-
-  useEffect(() => {
-    setPath(location.pathname);
-  }, [location]);
-
   return (
     <header
-      className={`sticky top-0 px-4 py-6 w-full ${
-        path === "/" ? "bg-white border-b border-gray-003 text-black" : ""
-      } transition-colors duration-150 ease-in-out z-50`}
+      className={
+        "sticky top-0 px-4 py-6 w-full bg-white border-b border-gray-003 shadow-md text-black transition-colors duration-150 ease-in-out z-50"
+      }
     >
       <nav className="flex justify-between my-0 mx-auto w-[90rem] h-[4rem] ">
         <LeftContainer />
